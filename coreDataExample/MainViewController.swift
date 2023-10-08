efc8f88f00c3e5c05142b0c7d9975f847c76342e
Igor Chernyshov <ichernyshov@bk.ru>
2023-10-08 11:50:27 +0300

    Initial Commit

---
+++ coreDataExample/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  coreDataExample
+//
+//  Created by Игорь Чернышов on 08.10.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
